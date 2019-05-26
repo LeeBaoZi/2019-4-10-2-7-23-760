eebc48d3cf7f3897159feddc84e04b402610a13f
LiBaozi <leepacesooocute@gmail.com>
2019-05-26 18:57:33 +0800

    third commit

--- main/main.js
+++ main/main.js
@@ -4,7 +4,7 @@ module.exports = function main(distance,parkTime) {
         return Math.round(6+(distance-2)*0.8+parkTime*0.25);
     }
     else if(distance>8){
-        return Math.round(6+(distance-2)*0.8+parkTime*0.25+(distance-8)*0.8*0.5)
+        return Math.round(6+(distance-2)*0.8+parkTime*0.25+(distance-8)*0.8*0.5);
     }
     else return Math.round(6+parkTime*0.25);
 };
