910b045e179baa9a0e3bdfbe561629985cb9d45c
LiBaozi <leepacesooocute@gmail.com>
2019-05-26 18:48:54 +0800

    second commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
-module.exports = function main(inputs) {
+module.exports = function main(distance,parkTime) {
     // write your code here...
-    return "price";
+    if(distance<=8&&distance>=2){
+        return Math.round(6+(distance-2)*0.8+parkTime*0.25);
+    }
+    else if(distance>8){
+        return Math.round(6+(distance-2)*0.8+parkTime*0.25+(distance-8)*0.8*0.5)
+    }
+    else return Math.round(6+parkTime*0.25);
 };
