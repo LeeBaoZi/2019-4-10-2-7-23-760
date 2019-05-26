ca4a25e675c0cfb3492900fc5e9ce2fd24ee38ae
LiBaozi <leepacesooocute@gmail.com>
2019-05-26 19:55:14 +0800

    fifth commit

--- main/main.js
+++ main/main.js
@@ -1,10 +1,10 @@
-module.exports = function main(distance,parkTime) {
+module.exports = function main(inputs) {
     // write your code here...
-    if(distance<=8&&distance>=2){
-        return Math.round(6+(distance-2)*0.8+parkTime*0.25);
+    if(inputs.distance<=8&&inputs.distance>=2){
+        return Math.round(6+(inputs.distance-2)*0.8+inputs.parkTime*0.25);
     }
-    else if(distance>8){
-        return Math.round(6+(distance-2)*0.8+parkTime*0.25+(distance-8)*0.8*0.5);
+    else if(inputs.distance>8){
+        return Math.round(6+(inputs.distance-2)*0.8+inputs.parkTime*0.25+(inputs.distance-8)*0.8*0.5);
     }
-    else return Math.round(6+parkTime*0.25);
+    else return Math.round(6+inputs.parkTime*0.25);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -3,37 +3,37 @@ const main = require('../main/main');
 describe('taxi fee', function () {
     it('within 2km and parking' , function() {
         let inputs = {distance:1,parkTime:5};
-        let summary = main(1,5);
+        let summary = main(inputs);
         let expected = 7;
         expect(summary).toEqual(expected);
     });
     it('between 2km and 8km and parking' , function() {
         let inputs = {distance:7,parkTime:10};
-        let summary = main(7,10);
+        let summary = main(inputs);
         let expected = 13;
         expect(summary).toEqual(expected);
     });
     it('more than 8km and parking' , function() {
         let inputs = {distance:10,parkTime:20};
-        let summary = main(10,20);
+        let summary = main(inputs);
         let expected = 18;
         expect(summary).toEqual(expected);
     });
     it('more than 8km and no parking' , function() {
         let inputs = {distance:15,parkTime:0};
-        let summary = main(15,0);
+        let summary = main(inputs);
         let expected = 19;
         expect(summary).toEqual(expected);
     });
     it('between 2km and 8km and no parking' , function() {
         let inputs = {distance:7,parkTime:0};
-        let summary = main(7,0);
+        let summary = main(inputs);
         let expected = 10;
         expect(summary).toEqual(expected);
     });
     it('within and no parking' , function() {
         let inputs = {distance:1,parkTime:0};
-        let summary = main(1,0);
+        let summary = main(inputs);
         let expected = 6;
         expect(summary).toEqual(expected);
     });
